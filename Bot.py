80bfaa623a324ded82cad06f0073b52cf3c72974
Adam Vilmin <crappyoats@gmail.com>
2017-02-01 12:18:27 -0600

    adding roll armchair

--- Bot.py
+++ Bot.py
@@ -1,15 +1,26 @@
 from flask import request, Flask
 from os import environ
 import json
+import requests
 
+# globals
 app = Flask(__name__)
+bot_id = environ['BOT_ID']
+
+
+def bot_commands(info):
+    if info['text'].startswith('HEY BOT'):
+        body = {"bot_id": bot_id,
+                "text": "Roll Armchair, " + info['name'] + "!!"}
+        requests.post("https://api.groupme.com/v3/bots/post", body)
 
 
 @app.route('/', methods=['POST'])
 def index():
     if request.method == "POST":
         data = json.loads(request.data.decode("utf-8"))
-        print(data["name"])
+        bot_commands(data)
+
     return "success"
 if __name__ == '__main__':
     port = int(environ.get('PORT', 5000))
