99c065d1a765f5a139efd8257db984d1afc83b15
Adam Vilmin <crappyoats@gmail.com>
2017-02-01 12:06:57 -0600

    testing

--- Bot.py
+++ Bot.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def index():
     if request.method == "POST":
-        data = json.load(request.data)
-        print(data["name"])
+        data = request.data
+        print(type(data))
     return "success"
 if __name__ == '__main__':
     port = int(environ.get('PORT', 5000))
